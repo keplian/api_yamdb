f1c7299e4eb116e82c43f36f310cea38960b92b4
keplian <keplian@gmail.com>
2021-06-15 23:26:58 +0300

    added User model in models.py

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "username",
+        "email",
+        "role",
+        "description",
+        "first_name",
+        "last_name",
+    )
+    search_fields = ("username", "email")
+    empty_value_display = "-пусто-"
--- api/models.py
+++ api/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class User(models.Model):
+    username_validator = UnicodeUsernameValidator()
+    username = models.CharField(
+        max_length=30, unique=True, validators=[username_validator]
+    )
+    email = models.EmailField(
+        _("email address"),
+    )
+    role = models.CharField(max_length=30)
+    description = models.CharField(max_length=150, blank=True)
+    first_name = models.CharField(_("first name"), max_length=30, blank=True)
+    last_name = models.CharField(_("last name"), max_length=150, blank=True)
