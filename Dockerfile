3ab6ce05d652383b5112ca9bb8c3726805e7bae0
keplian <keplian@gmail.com>
2021-09-01 19:57:53 +0300

    added docker and docker-compose

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM python:3.8.5
+
+# создать директорию /code
+RUN mkdir /code
+
+
+# скопировать файл requirements.txt из директории, в которой лежит докерфайл, в директорию /code
+COPY requirements.txt /code
+
+RUN ["pip3", "install", "--upgrade", "pip"]
+# выполнить команду (как в терминале, с тем же синтаксисом) для установки пакетов из requirements.txt
+RUN pip3 install -r /code/requirements.txt
+
+# скопировать всё содержимое директории, в которой лежит докерфайл, в директорию /code
+COPY . /code
+
+WORKDIR /code
+# при старте контейнера выполнить runserver
+CMD gunicorn api_yamdb.wsgi:application --bind 0.0.0.0:8000
--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -62,9 +62,13 @@ TEMPLATES = [
 WSGI_APPLICATION = "api_yamdb.wsgi.application"
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    'default': {
+        'ENGINE': os.environ.get('DB_ENGINE'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
@@ -95,7 +99,13 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/"),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+#
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/"),)
 
 CORS_URLS_REGEX = r"^/api/.*$"
 
